1bf00d92330a2702b2ce54efb504827cf6dc78ed
Rheena Blas <73996643+rheenablas@users.noreply.github.com>
2023-02-10 12:28:58 +0000

    scripts: add multiple scripts
    Pausemenu, levelLoader, Menu, Quiz

---
+++ Assets/Scripts/Quiz/AnswersData.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswersData : MonoBehaviour
+{
+    /*
+    // Instantiate the answers
+    [Header("UI Elements")]
+    [SerializeField] TextMeshProUGUI questionTextObject;
+    [SerializeField] Image toggle;
+
+    [Header("Sprites")]
+    [SerializeField] Sprite uncheckedToggle;
+    [SerializeField] Sprite checkedToggle;
+
+    private int _answerIndex = -1;
+    public int AnswerIndex { get { return _answerIndex; } }
+    */
+    public bool isCorrect = false;
+    public QuizManager quizManager;
+
+    public void Answer()
+    {
+        if (isCorrect)
+        {
+            Debug.Log("Correct");
+            quizManager.correct();
+            // Add score
+        }
+        else
+        {
+            Debug.Log("Incorrect");
+            //quizManager.correct();
+            // Remove score
+        }
+    }
+     
+}
---
+++ Assets/Scripts/Quiz/Question.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Questions// : ScriptableObject
+{
+    public string Question;
+    public string[] Answers;
+    public int CorrectAnswer;
+
+    /*[SerializeField] private string _questionText = string.Empty;
+    public string QuestionText { get { return _questionText; } }
+
+    [SerializeField] Answer[] _answerText = null;
+    public Answer[] Answers { get { return _answerText; } }
+
+    //parameters
+    [SerializeField] private bool _useTimer = false;
+    public bool UseTimer { get { return _useTimer; } }
+
+    [SerializeField] private int _timer = 0;
+    public int Timer { get { return _timer; } }
+
+    [SerializeField] private int _addScore = 10;
+    public int AddScore { get { return _addScore; } }
+
+    public List<int> GetCorrectAnswer()
+    {
+        List<int> correctAnswer = new List<int>();
+        for (int i = 0; i < Answers.Length; i++)
+        {
+            if (Answers[i].IsCorrect)
+            {
+                correctAnswer.Add(i);
+            }
+        }
+        return correctAnswer;
+    }*/
+
+}
+
+/*
+public struct Answer 
+{
+    [SerializeField] private string _answerText;
+    public string AnswerText { get { return _answerText; } }
+
+    [SerializeField] private bool _isCorrect;
+    public bool IsCorrect { get { return _isCorrect; } }
+}*/
