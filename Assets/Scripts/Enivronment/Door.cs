4e36d67949881246abf917bab4a0d7f74acd73a3
Colin <108523746@umail.ucc.ie>
2023-02-13 01:55:01 +0000

    script, fix: add Door script and fix WaypointFollower
    implements an interactive door feature and made changes to WaypointFollower for intro scene

---
+++ Assets/Scripts/Enivronment/Door.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class Door : Interactable
+{
+    public Sprite open;
+    public Sprite closed;
+
+    public SpriteRenderer sprite;
+    private bool isClosed;    
+
+    public override void Interact()
+    {
+        if (isClosed)
+        {
+            sprite.sprite = closed;
+            //Debug.Log("Closed");
+            GameObject.Find("Door_Closed_Bedroom").GetComponent<SpriteRenderer>().enabled = true;           
+            // TODO - Add scene change to Hallway
+            // TODO - Add sound effect
+        }
+        else
+        {
+            sprite.sprite = open;
+            //Debug.Log("Open");
+            GameObject.Find("Door_Closed_Bedroom").GetComponent<SpriteRenderer>().enabled = false;
+                  
+        }
+        isClosed = !isClosed;
+    }
+
+    private void start()
+    {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+        sprite.sprite = closed;
+        isClosed = false;
+        GameObject.Find("Door_Closed_Bedroom").GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
+
