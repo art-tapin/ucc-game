a1a9f42ed1405b93a289ea2465594426ede2cdb3
Daniil <122121660@umail.ucc.ie>
2023-02-26 17:59:28 +0000

    feat, fix: refactored Day/Night script, added timelapse for first cutscene.

--- Assets/LightEffect/DayNightCycle.cs
+++ Assets/LightEffect/DayNightCycle.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using UnityEngine.Playables;
+
 
 public class DayNightCycle : MonoBehaviour
 {
     public UnityEngine.Light directionalLight;
 
+    public float defaultTick = 1000;
     public float tick = 1000; // Increasing the tick, increases second rate
     public float seconds = 0;
-    public int mins = 0;
+    public float mins = 0;
     public int hours = 12;
     public int days = 1;
+    public bool timeLapse;
+    public float timeLapseMultiplier = 0.5f;
+    public PlayableDirector timeline;
+
 
     // [SerializeField] private Light lighter;
 
@@ -22,12 +29,30 @@ public class DayNightCycle : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate() // we used fixed update, since update is frame dependant. 
     {
+        isTimelineDone();
+
+        if (timeLapse)
+        {
+            tick = defaultTick * timeLapseMultiplier;
+        }
+        else 
+        {
+            tick = defaultTick;
+        }
+
         CalcTime();
     }
 
     public void CalcTime() // Used to calculate sec, min and hours
     {
-        seconds += Time.fixedDeltaTime * tick; // multiply time between fixed update by tick
+        if (timeLapse == false)
+        {
+            seconds += Time.fixedDeltaTime * tick; // multiply time between fixed update by tick
+        } 
+        else
+        {
+            mins += Time.fixedDeltaTime * tick;
+        } 
 
         if (seconds >= 60) // 60 sec = 1 min
         {
@@ -53,18 +78,32 @@ public class DayNightCycle : MonoBehaviour
     public void ControlDirectionalLight() // used to adjust the post processing slider.
     {
         
-        if (hours >= 21 && hours < 22) // dusk at 21:00 / 9pm    -   until 22:00 / 10pm
+        if (hours >= 12 && hours <= 18) // dusk at 21:00 / 9pm    -   until 22:00 / 10pm
         {
-            directionalLight.intensity = 1 - mins * 0.0167f; // since dusk is 1 hr, we just divide the mins by 60 which will slowly increase from 0 - 1 
+            directionalLight.intensity = 1 - (getMins() - 12 * 60) * 0.00277f; 
             //lighter.intensity = 4;
 
         }
-        if (hours >= 6 && hours < 7) // Dawn at 6:00 / 6am    -   until 7:00 / 7am
+        if (hours >= 6 && hours < 12) // Dawn at 6:00 / 6am    -   until 7:00 / 7am
         {
-            directionalLight.intensity = mins * 0.0167f; // we minus 1 because we want it to go from 1 - 0
+            directionalLight.intensity = (getMins() - 6 * 60) * 0.00277f;
             //lighter.intensity = 0;
         }
 
+    }
+
+    private float getMins()
+    {
+        return hours * 60 + mins;
+    }
+
+    public void isTimelineDone()
+    {
+         if (timeline.state != PlayState.Playing)
+        {
+            timeLapse = false;
+        }
+        else timeLapse = true;
     }
 }
 
