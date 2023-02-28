16760ef923b3039bc1c36883ec61638d7e0e0029
Colin <108523746@umail.ucc.ie>
2023-02-28 16:57:58 +0000

    script: add QuizTrigger script for WWTBAM game

---
+++ Assets/QuizTrigger.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizTrigger : MonoBehaviour
+{
+   public GameObject quiz;
+   public Camera cam1;
+   public Camera cam2;
+   public GameObject table;
+   public GameObject platform;
+   public GameObject curtain;
+   public Animator animator;
+
+   private void OnTriggerEnter2D(Collider2D collision) {
+
+    if (collision.gameObject.CompareTag("Player") == true)
+    {
+        //cam1.Camera.SetActive(false);
+        cam1.enabled = false;
+        //cam2.Camera.SetActive(true);
+        cam2.enabled = true;
+        table.GetComponent<WaypointFollower>().enabled = true;
+        platform.GetComponent<WaypointFollower>().enabled = true;
+        curtain.GetComponent<Animator>().enabled = true;
+        // delay the quiz from appearing
+        //table.SetActive(true);
+
+        quiz.SetActive(true);
+        animator.SetBool("isOpen", true);
+        
+        //Destroy(gameObject, .5f);           
+    }
+   }
+
+   private void Start() {
+       quiz.SetActive(false);
+       //cam1.Camera.SetActive(true);
+       cam1.enabled = true;
+       cam2.enabled = false;
+       //cam2.Camera.SetActive(false);
+       
+
+       table.GetComponent<WaypointFollower>().enabled = false;
+        platform.GetComponent<WaypointFollower>().enabled = false;
+        curtain.GetComponent<Animator>().enabled = false;
+        animator.SetBool("isOpen", false);
+       //table.SetActive(false);
+}
+}
