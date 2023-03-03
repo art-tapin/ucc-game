3c3bafbc52a019c5ccb63eb88b2471409a5467ed
Anya <122121663@umail.ucc.ie>
2023-03-03 19:06:35 +0000

    fix: fix camera zoom ouz running after main menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -7,6 +7,7 @@ public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
+    public Animator animator;
     //public AudioSource wwtbamAudio;
 
     // Update is called once per frame
@@ -30,6 +31,7 @@ public class PauseMenu : MonoBehaviour
     {   Debug.Log("resume");
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
+        animator.SetBool("zoomOutScene", false);
         GameIsPaused = false;
         //wwtbamAudio.Play();
     }
