74c9a9ced1c02c279b63ff051f39feea170dfd13
Colin <108523746@umail.ucc.ie>
2023-03-06 13:31:01 +0000

    Auto stash before revert of "fix: fix camera movement"

--- Assets/Scripts/Menu/SceneMenu.cs
+++ Assets/Scripts/Menu/SceneMenu.cs
@@ -5,14 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class SceneMenu : MonoBehaviour
 {
+
+    //private bool isPlayClicked;
+    //public Animator animator;
+
     public void Scene1()
     {
         SceneManager.LoadScene(1);
+        //isPlayClicked = PlayerPrefs.GetInt("ZoomOutScene", 0) == 1;
+        //animator.SetBool("zoomOutScene", !isPlayClicked);
+        //Debug.Log("%%%%%%%%%%%%%%%%%%%%%%%%%%%");
     }
 
     public void Scene2()
     {
         SceneManager.LoadScene(2);
+       // PlayerPrefs.SetInt("ZoomOutScene", 1);
     }
 
     public void Scene3()
