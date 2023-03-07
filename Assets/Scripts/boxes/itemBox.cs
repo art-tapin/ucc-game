2e5ca22c0df32283500911455c91fc10671eb20d
Ivanoherlihy <117639027+Ivanoherlihy@users.noreply.github.com>
2023-03-07 20:19:02 +0000

    add order and inventory system

---
+++ Assets/Scripts/boxes/itemBox.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class itemBox : MonoBehaviour
+{ 
+    private bool open = true;
+
+    public string boxType;
+    private orderSpawner orderSpawner;
+    private inventory inventory;
+    private SpriteRenderer sprite;
+    private GameObject player;
+    private BoxCollider2D playerbox;
+    private BoxCollider2D boxcollider; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        orderSpawner = FindObjectOfType<orderSpawner>();
+        inventory= FindObjectOfType<inventory>();
+        boxcollider = GetComponent<BoxCollider2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerbox = player.GetComponent<BoxCollider2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (boxcollider.IsTouching(playerbox) && open) 
+        {
+            open=false;
+            switch (boxType)
+            {
+                case "addBurgers":
+                    inventory.increaseBurger(1);
+                    break;
+                case "addChips":
+                    inventory.increaseChips(1);
+                    break;
+                case "addMilkshakes":
+                    inventory.increaseMilkshake(1);
+                    break;
+                case "check":
+                    orderSpawner.check();
+                    break;
+                case "empty":
+                    inventory.empty();
+                    break;
+            
+            }
+        }
+        if (!open && !boxcollider.IsTouching(playerbox)){
+            open = true;
+
+        }
+    } 
+}
