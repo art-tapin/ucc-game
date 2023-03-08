586d33e0c6c868469aaeb0ecc8d89889f33ebb83
Rheena Blas <73996643+rheenablas@users.noreply.github.com>
2023-03-08 17:45:18 +0000

    script: add blinking text on play button on characterSelection

--- Assets/Scripts/levelLoaderGender.cs
+++ Assets/Scripts/levelLoaderGender.cs
@@ -8,13 +8,11 @@ public class levelLoaderGender : MonoBehaviour
 {
     public Animator transition;
     public float transitionTime = 1f;
-    private bool gender = false;
     private int buildIndexConstant = 1;
 
     // Update is called once per frame
     public void SetGender (int qualityIndex)
     {
-        
         if (qualityIndex != 0)
         {
             buildIndexConstant = 2;
