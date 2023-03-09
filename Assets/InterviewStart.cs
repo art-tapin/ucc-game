f586a696f8219e52721a674820921630f1caa554
Colin <108523746@umail.ucc.ie>
2023-03-09 03:22:13 +0000

    scripts: add scripts for mini-game

---
+++ Assets/InterviewStart.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterviewStart : MonoBehaviour
+{
+
+
+    public GameObject player;
+    public GameObject interview;
+    public GameObject bowser;
+    public Animator bowserAnim;
+
+    public AudioSource interviewAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            player.GetComponent<Animator>().SetInteger("state", 0);
+            player.GetComponent<PlayerMovement>().enabled = false;
+            interview.SetActive(true);
+            bowser.SetActive(true);
+            //bowserAnim.SetBool("isTalking", true);
+            //interviewAudio.Play();
+        }
+    }
+}
