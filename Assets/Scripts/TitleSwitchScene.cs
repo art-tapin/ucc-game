299be9f43f137385ef31b7502c0cf6e3e93dc2d8
Rheena Blas <73996643+rheenablas@users.noreply.github.com>
2023-03-11 23:44:17 +0000

    animations: added transition on the titleScene:2

---
+++ Assets/Scripts/TitleSwitchScene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSwitchScene : MonoBehaviour
+{
+    public GameObject animationStart;
+    public GameObject animationEnd;
+    public int sceneIndex = 1;
+    
+    void Start()
+    {
+        animationStart.SetActive(true);
+        StartCoroutine(StartAnimation());
+    }
+    
+    IEnumerator StartAnimation()
+    {
+        yield return new WaitForSeconds(5);
+        animationEnd.SetActive(true);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + sceneIndex);
+    }
+}
