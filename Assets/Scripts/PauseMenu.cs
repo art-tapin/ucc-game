40c3de54455f9590b34f1b6bcdf4439f4f9ecd08
Rheena Blas <73996643+rheenablas@users.noreply.github.com>
2023-03-12 00:02:39 +0000

    fix: edit bug to redirect to main menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -25,28 +25,24 @@ public class PauseMenu : MonoBehaviour
     }
 
     public void Resume()
-    {   Debug.Log("resume");
+    {   
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
-        //wwtbamAudio.Play();
     }
 
     public void Pause()
     {
-        Debug.Log("pause");
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
-        //wwtbamAudio.Pause();
-
     }
 
     public void LoadMenu()
     {
         Debug.Log("Loading Menu...");
         Time.timeScale = 1f;
-        SceneManager.LoadScene("Scenes/1_MainMenu");
+        SceneManager.LoadScene("Scenes/1_1_MainMenu");
     }
 
     public void QuitGame()
